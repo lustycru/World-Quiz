b2588ab2120f9aa71f2d2ba861d2fb48fd5cd9ba
Jules NEVEU <jneveu@edenschool.fr>
2024-04-11 14:23:39 +0200

    2
    2

--- assets/js/script.js
+++
@@ -1,4 +0,0 @@
-document.querySelector('.scroll-snap').addEventListener('wheel', (event) => {
-    event.preventDefault();
-    document.querySelector('.scroll-snap').scrollLeft += event.deltaY;
-});
