220755807e8311e2b8843abce9a366dde8d51a2b
Thomas Dimnet <thomas.dimnet@gmail.com>
2024-04-12 09:55:22 +0200

    Clean code et random array

--- World-Quiz/assets/js/quiz.js
+++ World-Quiz/assets/js/quiz.js
@@ -6,19 +6,34 @@ const questionC = document.querySelector('.question-C')
 const questionD = document.querySelector('.question-D')
 
 
+function shuffle(array) {
+  array.sort(() => Math.random() - 0.5);
+}
+
 
 const requestOptions = {
     method: "GET",
     redirect: "follow"
   };
   
-  fetch("https://quizzapi.jomoreschi.fr/api/v1/quiz?limit=1", requestOptions)
+  fetch("https://quizzapi.jomoreschi.fr/api/v1/quiz?limit=10", requestOptions)
     .then((response) => response.json())
     .then(function (result){
       console.log(result)
       apiQuestion.innerHTML = result.quizzes[0].question;
       apiQuestion.style = "width: 40%";
       apiCategorie.innerHTML = result.quizzes[0].category;
+
+
+      console.log(result.quizzes[0])
+      console.log(result.quizzes[0].answer)
+      console.log(result.quizzes[0].badAnswers)
+
+      const answers = [result.quizzes[0].answer, ...result.quizzes[0].badAnswers]
+      shuffle(answers)
+
+      console.log(answers)
+
     })
 
       // quizzes[0].answer.addEventListener('click', )
