ef88a933eebf8f5c1f9e38df773e0408759646ac
Dadju Djuna Nsungula (officiellement officiel) <halmeida@edenschool.fr>
2024-04-12 12:01:55 +0200

    NE PAS TOUCHER FAUT JE REGLE DES TRUC AVANT !!!!!!!!!!!!!!!!!!!!!!!

---
+++ World-Quiz/assets/js/choises.js
@@ -0,0 +1,143 @@
+
+const BoxCulture = document.querySelector('#culture')
+const BoxActu = document.querySelector('#actu')
+const BoxSport = document.querySelector('#sport')
+const BoxMusique = document.querySelector('#musique')
+const BoxArt = document.querySelector('#art')
+const BoxTv = document.querySelector('#tv')  
+const content = document.querySelector('.content')
+const box = document.querySelectorAll('.box')
+const main = document.querySelector('main')
+const header = document.querySelector('header')
+const h2Choix = document.querySelector('h2')
+const contact = document.querySelector('.contact')
+
+const Diffdiv = document.createElement('div')
+Diffdiv.classList.add('diff')
+
+const divChoises = document.createElement('div')
+divChoises.classList.add('divChoises')
+Diffdiv.appendChild(divChoises)
+
+const Diffh3 = document.createElement('h3')
+Diffh3.classList.add('diffh3')
+Diffh3.textContent = 'Choissez le niveau des questions :';
+Diffdiv.appendChild(Diffh3)
+
+const niv1 = document.createElement('div')
+niv1.classList.add('niv1')
+niv1.classList.add('boxNiv')
+divChoises.appendChild(niv1)
+
+const pNiv1 = document.createElement('p')
+pNiv1.classList.add('pNiv1')
+pNiv1.textContent = 'Facile';
+niv1.appendChild(pNiv1)
+
+
+const niv2 = document.createElement('div')
+niv2.classList.add('niv2')
+niv2.classList.add('boxNiv')
+divChoises.appendChild(niv2)
+
+const pNiv2 = document.createElement('p')
+pNiv2.classList.add('pNiv2')
+pNiv2.textContent = 'Normal';
+niv2.appendChild(pNiv2)
+
+
+const niv3 = document.createElement('div')
+niv3.classList.add('niv3')
+niv3.classList.add('boxNiv')
+divChoises.appendChild(niv3)
+
+const pNiv3 = document.createElement('p')
+pNiv3.classList.add('pNiv3')
+pNiv3.textContent = 'Difficile';
+niv3.appendChild(pNiv3)
+
+
+
+console.log(header)
+
+
+
+
+BoxCulture.addEventListener('click', function () {
+
+    const categorie = "culture_generale"
+    console.log(categorie)
+    main.removeChild(content)
+    main.appendChild(Diffdiv)
+    
+
+})
+
+BoxActu.addEventListener('click', function () {
+
+    const categorie = "actu_politique"
+    console.log(categorie)
+    main.removeChild(content)
+    main.appendChild(Diffdiv)
+})
+
+BoxSport.addEventListener('click', function () {
+
+    const categorie = "sport"
+    console.log(categorie)
+    main.removeChild(content)
+    main.appendChild(Diffdiv)
+
+})
+
+BoxMusique.addEventListener('click', function () {
+
+    const categorie = "musique"
+    console.log(categorie)
+    main.removeChild(content)
+    main.appendChild(Diffdiv)
+
+})
+
+BoxArt.addEventListener('click', function () {
+
+    const categorie = "art_litterature"
+    console.log(categorie)
+    main.removeChild(content)
+    main.appendChild(Diffdiv)
+
+})
+
+BoxTv.addEventListener('click', function () {
+
+    const categorie = "tv_cinéma"
+    console.log(categorie)
+    main.removeChild(content)
+    main.appendChild(Diffdiv)
+
+})
+
+niv1.addEventListener('click', function () {
+
+    const niv = "facile"
+    header.removeChild(h2Choix)
+    header.removeChild(contact)
+
+})
+
+niv2.addEventListener('click', function () {
+
+    const niv = "normal"
+    header.removeChild(h2Choix)
+    header.removeChild(contact)
+
+})
+
+niv3.addEventListener('click', function () {
+
+    const niv3 = "difficile"
+    header.removeChild(h2Choix)
+    header.removeChild(contact)
+    
+
+})
--- World-Quiz/assets/js/quiz.js
+++ World-Quiz/assets/js/quiz.js
@@ -1,5 +1,6 @@
 const apiQuestion = document.querySelector('.question-api')
 const apiCategorie = document.querySelector('.categorie-api')
+const apiNiv = document.querySelector('.niv-api')
 const questionA = document.querySelector('.question-A')
 const questionB = document.querySelector('.question-B')
 const questionC = document.querySelector('.question-C')
@@ -12,28 +13,40 @@ function shuffle(array) {
 
 
 const requestOptions = {
-    method: "GET",
-    redirect: "follow"
-  };
-  
-  fetch("https://quizzapi.jomoreschi.fr/api/v1/quiz?limit=10", requestOptions)
-    .then((response) => response.json())
-    .then(function (result){
-      console.log(result)
-      apiQuestion.innerHTML = result.quizzes[0].question;
+  method: "GET",
+  redirect: "follow"
+};
+
+fetch("https://quizzapi.jomoreschi.fr/api/v1/quiz?limit=1", requestOptions)
+  .then((response) => response.json())
+  .then(function (result) {
+    console.log(result)
+
+    for (let i = 0; i < 10; i++) {
+      apiQuestion.innerHTML = `Question : ${result.quizzes[0].question}`;
       apiQuestion.style = "width: 40%";
-      apiCategorie.innerHTML = result.quizzes[0].category;
-
-
-      console.log(result.quizzes[0])
-      console.log(result.quizzes[0].answer)
-      console.log(result.quizzes[0].badAnswers)
+      apiCategorie.innerHTML = `Catégorie : ${result.quizzes[0].category}`;
+      apiNiv.innerHTML = ` Difficulté : ${result.quizzes[0].difficulty}`;
 
       const answers = [result.quizzes[0].answer, ...result.quizzes[0].badAnswers]
       shuffle(answers)
 
-      console.log(answers)
+      questionA.textContent = ` A : ${answers[0]}`
+      questionB.textContent = ` B : ${answers[1]}`
+      questionC.textContent = ` C : ${answers[2]}`
+      questionD.textContent = ` D : ${answers[3]}`
 
-    })
+    }
 
-      // quizzes[0].answer.addEventListener('click', )
+
+
+
+
+    console.log(yes)
+    console.log(result.quizzes[0])
+    console.log(result.quizzes[0].answer)
+    console.log(result.quizzes[0].badAnswers)
+
+  })
+
+// quizzes[0].answer.addEventListener('click', )
