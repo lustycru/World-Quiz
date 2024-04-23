2c63695ce35a6b68ad4930dec4d6ddd17d35587f
Jules NEVEU <jneveu@edenschool.fr>
2024-04-23 13:53:44 +0200

    "
    "

--- World-Quiz/assets/js/script.js
+++ World-Quiz/assets/js/script.js
@@ -1,4 +1,14 @@
+const randomCateg = document.querySelector('.bandeau a')
+
 document.querySelector(".scroll-snap").addEventListener("wheel", (event) => {
   event.preventDefault();
   document.querySelector(".scroll-snap").scrollLeft += event.deltaY;
 });
+
+randomCateg.addEventListener('click', function(){
+  fetch(`https://quizzapi.jomoreschi.fr/api/v1/quiz?limit=5&category=${categorie}&difficulty=${niv}`)
+  .then((response) => response.json())
+  .then(function (result) {
+    
+  })
+})
